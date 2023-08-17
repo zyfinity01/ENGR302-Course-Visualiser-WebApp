5117644fe8552a504537b6ff593db291e0474193
Jonty Morris <jonty.morris@yahoo.co.nz>
2023-08-17 14:48:03 +1200

    Parse more fields and limit to ENGR

--- backend/src/services/catprintService.ts
+++ backend/src/services/catprintService.ts
@@ -3,16 +3,21 @@ import { Course } from '../models/course'
 
 export async function getAllCourses() {
   const [resultsU, resultsP] = await Promise.all([
-    getCoursesForProgramme('u'), // undergraduate
-    getCoursesForProgramme('p'), // postgraduate
+    getCoursesForProgramme('Engineering', 'u'),
+    getCoursesForProgramme('Engineering', 'p'),
+    getCoursesForProgramme('Computer%20Science', 'u'),
+    getCoursesForProgramme('Computer%20Science', 'p'),
   ])
 
   return [...resultsU, ...resultsP]
 }
 
-async function getCoursesForProgramme(programme: string): Promise<Course[]> {
+async function getCoursesForProgramme(
+  directory: string,
+  programme: string
+): Promise<Course[]> {
   const year = new Date().getFullYear()
-  const url = `https://service-web.wgtn.ac.nz/dotnet2/catprint.aspx?t=${programme}${year}`
+  const url = `https://service-web.wgtn.ac.nz/dotnet2/catprint.aspx?t=${programme}${year}&d=${directory}`
 
   const response = await fetch(url)
   const html = await response.text()
@@ -24,24 +29,20 @@ async function getCoursesForProgramme(programme: string): Promise<Course[]> {
 
   const results: Course[] = []
   for (let i = 0; i < courseIds.length; i++) {
-    const courseIdAndName = courseIds[i].textContent
-    const courseSplit = courseIdAndName.indexOf('–')
-
-    const pointsAndRequirements = coursePoints[i].textContent
-    const pointSplit = pointsAndRequirements.indexOf('•')
+    const courseIdAndName = courseIds[i].textContent.split('–')
+    const pointsAndRequirements = coursePoints[i].textContent.split('•')
+    const points = parseInt(pointsAndRequirements[0].replace(' pts', '').trim())
+    const requirements =
+      pointsAndRequirements.length > 1 ? pointsAndRequirements[1].trim() : ''
+    const level = parseInt(courseIdAndName[0].split(' ')[1][0] + '00')
 
     results.push({
-      id: courseIdAndName.substring(0, courseSplit).trim(),
-      name: courseIdAndName.substring(courseSplit + 1).trim(),
+      id: courseIdAndName[0].trim(),
+      name: courseIdAndName[1].trim(),
       description: subjectBodies[i].textContent,
-      points:
-        pointSplit == -1
-          ? pointsAndRequirements
-          : pointsAndRequirements.substring(0, pointSplit).trim(),
-      requirements:
-        pointSplit == -1
-          ? ''
-          : pointsAndRequirements.substring(pointSplit + 1).trim(),
+      requirements: requirements,
+      points: points,
+      level: level,
     })
   }
 
