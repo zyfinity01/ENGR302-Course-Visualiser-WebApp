bac594ca92d4ba51b5a4dddd11642b32e7715b34
Sam Redmond <sam.redmond@fingermarkglobal.com>
2023-08-22 17:51:09 +1200

    change to use tailwind and material-tailwind

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Collapse, Navbar, NavbarToggler, Container } from 'reactstrap';
 import { faFileExport } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
-    <div className="nav-container mb-4">
-      <Navbar style={{ backgroundColor: '#004b34' }} light expand="md">
-        <Container>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="flex-grow-0">
-              <img src="/viclogo.png" alt="loading.svg" className="img-fluid" style={{ maxWidth: '150px' }} />
-            </div>
-            <h1 className="flex-grow-1 text-white p-1 text-center ml-8 offset-md-2" style={{ backgroundColor: 'black' }}>
-              Course Visualiser
-            </h1>
-            <div className="flex-grow-1">
-              <button className="text-white border-0 text-center offset-md-8" style={{ backgroundColor: '#004b34' }}>
-                Export <FontAwesomeIcon icon={faFileExport} className="ml-2" />
-              </button>
-            </div>
-          </div>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            {/* Nav content here */}
-          </Collapse>
-        </Container>
-      </Navbar>
+    <div className='px-5 flex md:grid md:grid-cols-4 w-full bg-[#004b34] h-20 text-white'>
+      {/* Logo */}
+      <div className="flex justify-center items-center hidden md:flex">
+        <img src="/viclogo.png" alt="Logo" className="max-w-[150px] mr-auto" />
+      </div>
+      {/* Text */}
+      <div className="flex justify-center items-center col-span-2">
+        <h1 className='text-center text-2xl md:text-3xl'>Course Visualiser</h1>
+      </div>
+      {/* Export Button */}
+      <button className="flex justify-center items-center ml-auto">
+        Export <FontAwesomeIcon icon={faFileExport} className="ml-2" />
+      </button>
     </div>
   );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Auth0Provider, CacheLocation } from '@auth0/auth0-react'
+import { ThemeProvider } from "@material-tailwind/react";
 
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-
-const providerConfig = {
-  domain: process.env.REACT_APP_AUTH_DOMAIN as string,
-  clientId: process.env.REACT_APP_AUTH_CLIENT_ID as string,
-  cacheLocation: 'localstorage' as CacheLocation,
-  authorizationParams: {
-    redirect_uri: window.location.origin,
-    audience: process.env.REACT_APP_AUTH_AUDIENCE,
-  },
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Auth0Provider {...providerConfig}>
+    <ThemeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Auth0Provider>
+    </ThemeProvider>
   </React.StrictMode>
 )
